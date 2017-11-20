f88c196789f93d1b43138480146d78ed4161a441
Jens Freudenau <jens@freude-now.de>
2017-11-20 10:16:09 +0100

    archive folder

--- resources/views/front/records/best.blade.php
+++ resources/views/front/records/best.blade.php
@@ -5,7 +5,7 @@
             <h3>{{ $header }}</h3>
             <div class="list-group">
             	@forelse($bests as $best)
-                    <a href="{{ asset('bestenliste/KBL_'.$best->year.'_'.$file.'.pdf') }}" target="_blank" class="list-group-item">{{$best->year}}</a>
+                    <a href="{{ asset('storage/bestenliste/KBL_'.$best->year.'_'.$file.'.pdf') }}" target="_blank" class="list-group-item">{{$best->year}}</a>
                 @empty
                     <p class="desc">keine Rekorde.</p>
             @endforelse
