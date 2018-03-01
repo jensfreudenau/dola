a8577a87f0dc2654c76dac0ea83411faa597432c
Jens Freudenau <jens@freude-now.de>
2018-03-01 13:59:14 +0100

    style

--- resources/views/emails/registration.blade.php
+++ resources/views/emails/registration.blade.php
@@ -12,36 +12,38 @@
     <dt>Verein:</dt>
     <dd>{{ $announciator->clubname }}</dd>
     @if($announciator->resultlist)
-        <dt>es ist eine Ergebnisliste angefordert worden:</dt><dd></dd>
+        <dt>es ist eine Ergebnisliste angefordert worden:</dt>
+        <dd></dd>
         <dt>Strasse:</dt>
         <dd>{{ $announciator->street }}</dd>
         <dt>Ort:</dt>
         <dd>{{ $announciator->city }}</dd>
     @endif
 </dl>
-
-<table class="table table-hover">
-    <thead class="thead-inverse">
-    <tr>
-        <th>Vorname</th>
-        <th>Nachname</th>
-        <th>Geb. Jahr</th>
-        <th>Altersklasse</th>
-        <th>Disziplin</th>
-        <th>Bestzeit</th>
-    </tr>
-    </thead>
-    <tbody>
-
-    @foreach($announciator->Participator as $participator)
+<div class="table-responsive">
+    <table class="table table-hover">
+        <thead class="thead-inverse">
         <tr>
-            <td>{{$participator->prename}}</td>
-            <td>{{$participator->lastname }}</td>
-            <td>{{$participator->birthyear }}</td>
-            <td>{{$participator->ageclass->shortname }}</td>
-            <td>{{$participator->discipline->shortname }}</td>
-            <td>{{$participator->best_time}}</td>
+            <th>Vorname</th>
+            <th>Nachname</th>
+            <th>Geb. Jahr</th>
+            <th>Altersklasse</th>
+            <th>Disziplin</th>
+            <th>Bestzeit</th>
         </tr>
-    @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+
+        @foreach($announciator->Participator as $participator)
+            <tr>
+                <td>{{$participator->prename}}</td>
+                <td>{{$participator->lastname }}</td>
+                <td>{{$participator->birthyear }}</td>
+                <td>{{$participator->ageclass->shortname }}</td>
+                <td>{{$participator->discipline->shortname }}</td>
+                <td>{{$participator->best_time}}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
