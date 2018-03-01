3867295bfb0f78cdee395f7f8c36df4edea4a4c7
Jens Freudenau <jens@freude-now.de>
2018-03-01 13:59:14 +0100

    style

--- resources/views/front/announciators/list.blade.php
+++ resources/views/front/announciators/list.blade.php
@@ -1,39 +1,41 @@
 @extends('layouts.front')
 @section('content')
     <div class="card card-default mb-5 p-3">
-    <h3>{{ $list['competition']->header }}</h3>
-    <dl class="dl-horizontal">
-        <dt>Datum:</dt>
-        <dd>{{ $list['competition']->start_date }}</dd>
-        <dt>Veranstalter:</dt>
-        <dd>{{ $list['competition']->organizer->name }}</dd>
-        <dd>{{ $list['competition']->organizer->address->name }}</dd>
-        <dd>{{ $list['competition']->organizer->address->telephone }}</dd>
-        <dd>{{ $list['competition']->organizer->address->email }}</dd>
-    </dl>
-    <table class="table table-hover">
-        <thead class="thead-inverse">
-        <tr>
-            <th>Vorname</th>
-            <th>Nachname</th>
-            <th>Geb. Jahr</th>
-            <th>Altersklasse</th>
-            <th>Disziplin</th>
-            <th>Bestzeit</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($list['announciator']->Participator as $participator)
-            <tr>
-                <td>{{$participator->prename}}</td>
-                <td>{{$participator['lastname'] }}</td>
-                <td>{{$participator['birthyear'] }}</td>
-                <td>{{$participator->ageclass->shortname }}</td>
-                <td>{{$participator->discipline->shortname }}</td>
-                <td>{{$participator['best_time'] }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+        <h3>{{ $list['competition']->header }}</h3>
+        <dl class="dl-horizontal">
+            <dt>Datum:</dt>
+            <dd>{{ $list['competition']->start_date }}</dd>
+            <dt>Veranstalter:</dt>
+            <dd>{{ $list['competition']->organizer->name }}</dd>
+            <dd>{{ $list['competition']->organizer->address->name }}</dd>
+            <dd>{{ $list['competition']->organizer->address->telephone }}</dd>
+            <dd>{{ $list['competition']->organizer->address->email }}</dd>
+        </dl>
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead class="thead-inverse">
+                <tr>
+                    <th>Vorname</th>
+                    <th>Nachname</th>
+                    <th>Geb. Jahr</th>
+                    <th>Altersklasse</th>
+                    <th>Disziplin</th>
+                    <th>Bestzeit</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($list['announciator']->Participator as $participator)
+                    <tr>
+                        <td>{{$participator->prename}}</td>
+                        <td>{{$participator['lastname'] }}</td>
+                        <td>{{$participator['birthyear'] }}</td>
+                        <td>{{$participator->ageclass->shortname }}</td>
+                        <td>{{$participator->discipline->shortname }}</td>
+                        <td>{{$participator['best_time'] }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
