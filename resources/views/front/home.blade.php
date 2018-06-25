2c6a3e1369590f6234c65886eb2d1028055b46f4
Jens Freudenau <jens@freude-now.de>
2018-06-25 16:20:34 +0200

    anzahl wettkämpfe

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.front')
 @section('content')
     <div class="card card-default mb-5 p-5">
-        <h2 class="pb-3">Die nächsten {{count($competitions)}} Wettk&auml;mpfe</h2>
+        @php
+            $amountCompis = count($competitions);
+        @endphp
+        @if($amountCompis > 1)
+            <h2 class="pb-3">Die nächsten {{$amountCompis}} Wettk&auml;mpfe</h2>
+        @elseif($amountCompis == 1)
+            <h2 class="pb-3">Der nächste Wettkampf</h2>
+        @else
+            <h2 class="pb-3">Zurzeit keine Wettk&auml;mpfe</h2>
+        @endif
         <hr>
         <ul>
             @forelse($competitions as $competition)
